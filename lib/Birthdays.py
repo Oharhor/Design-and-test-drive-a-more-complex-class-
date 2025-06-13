031fc973b995ca088adf80fac96c83c48b1e217f
Osama Harhor <oharhor22@gmail.com>
2025-06-13 15:37:21 +0100

    Third Commit

--- lib/Birthdays.py
+++ lib/Birthdays.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 class Birthdays():
     def __init__(self):
         self.dict = {}
@@ -8,4 +9,22 @@ class Birthdays():
             raise Exception ("No one found with that name, did you misspell?")
         else:
             self.dict.update({name:new_date})
+    def update_name(self,old_name,new_name):
+        if old_name not in self.dict.keys():
+            raise Exception ("No one found with that name, did you misspell?")
+        else:
+            self.dict[new_name] = self.dict.pop(old_name)
+    def coming_soon(self):
+        empty_list = []
+        for dates in self.dict.values():
+            #formatted = [dates.strftime('%Y-%m-%d')]
+            checking = datetime.now() - datetime.strptime(dates,'%d,%m,%Y')
+            if checking.days%365 < 30:
+                key = next((k for k, v in self.dict.items() if v == dates), None)
+                empty_list.append({key,dates[0:5]})
+        return empty_list
+
+# Myfriends = Birthdays()
+# Myfriends.add("Jasmine","15,06,2003")
+# Myfriends.coming_soon()
     
--- tests/test_birthday.py
+++ tests/test_birthday.py
@@ -27,27 +27,29 @@ def test_birthday_date_update_misspelled_names():
     error_message = str(e.value)
     assert error_message == "No one found with that name, did you misspell?"
 # ## Updating a persons' name
-# def test_birthday_name_update():
-#     Myfriends = Birthdays()
-#     Myfriends.add("Deepaankar","24,09,2003")
-#     Myfriends.update_name("Deepankar""Daniel")
-#     assert myfriends.dict == {"Daniel","24,09,2003"}
-# ## Updating persons name but no name found
-# def test_birthday_name_update_name_not_found():
-#     Myfriends = Birthdays()
-#     Myfriends.add("Deepaankar","24,09,2003")
-#     Myfriends.update_name("Deepankar""Daniel")
-#     assert Myfriends.update_name("Deepankar""Daniel") == "No one found with that name, did you misspell?"
+def test_birthday_name_update():
+    Myfriends = Birthdays()
+    Myfriends.add("Deepankar","24,09,2003")
+    Myfriends.update_name("Deepankar","Daniel")
+    assert Myfriends.dict == {"Daniel":"24,09,2003"}
+## Updating persons name but no name found
+def test_birthday_name_update_name_not_found():
+    Myfriends = Birthdays()
+    Myfriends.add("Deepankar","24,09,2003")
+    with pytest.raises(Exception) as e:
+        Myfriends.update_name("Deepankar1","Daniel")
+    error_message = str(e.value)
+    assert error_message == "No one found with that name, did you misspell?"
 
-# ## Calculate and output a list of whose birthdays from dictionary is coming soon
-# def test_birthday_coming_soon():
-#     Myfriends = Birthdays()
-#     Myfriends.add("Jasmine","15,06,2003")
-#     assert Myfriends.coming_soon() == [Jasmine,15th June] 
-# ## Calculate and output a list of multiple friends birthdays from dictionary is coming soon excluding those who aren't soon
-# def test_birthday_coming_soon_multiple():
-#     Myfriends = Birthdays()
-#     Myfriends.add("Jasmine","15,06,2003")
-#     Myfriends.add("Mohammed","18,06,2003")
-#     Myfriends.add("Harry","15,12,2003")
-#     assert Myfriends.coming_soon() == [[Jasmine,15th June],[Mohammed,18th June]]
+## Calculate and output a list of whose birthdays from dictionary is coming soon
+def test_birthday_coming_soon():
+    Myfriends = Birthdays()
+    Myfriends.add("Jasmine","15,06,2003")
+    assert Myfriends.coming_soon() == [{'Jasmine','15,06'}] 
+## Calculate and output a list of multiple friends birthdays from dictionary is coming soon excluding those who aren't soon
+def test_birthday_coming_soon_multiple():
+    Myfriends = Birthdays()
+    Myfriends.add("Jasmine","15,06,2003")
+    Myfriends.add("Mohammed","18,06,2003")
+    Myfriends.add("Harry","15,12,2003")
+    assert Myfriends.coming_soon() == [{'Jasmine','15,06'},{'Mohammed','18,06'}]
